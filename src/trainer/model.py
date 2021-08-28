0d5319c8d40b7400820d9b13fa588346f3b2b9e3
mikeheddes <mikeheddes@gmail.com>
2021-08-28 16:24:52 -0700

    Cartpole training

--- src/trainer/model.py
+++ src/trainer/model.py
@@ -11,31 +11,32 @@ class DQN(nn.Module):
         super(DQN, self).__init__()
         C, H, W = frame_shape
 
-        self.conv1 = nn.Conv2d(
-            in_channels=C, out_channels=16, kernel_size=8, stride=4, padding=2)
-        H = get_conv_output_size(H, 8, stride=4, padding=2)
-        W = get_conv_output_size(W, 8, stride=4, padding=2)
+        # self.conv1 = nn.Conv2d(
+        #     in_channels=C, out_channels=16, kernel_size=8, stride=4, padding=2)
+        # H = get_conv_output_size(H, 8, stride=4, padding=2)
+        # W = get_conv_output_size(W, 8, stride=4, padding=2)
 
-        self.conv2 = nn.Conv2d(
-            in_channels=16, out_channels=32, kernel_size=4, stride=2, padding=1)
-        H = get_conv_output_size(H, 4, stride=2, padding=1)
-        W = get_conv_output_size(W, 4, stride=2, padding=1)
-        self.num_conv2_features = 32 * H * W
+        # self.conv2 = nn.Conv2d(
+        #     in_channels=16, out_channels=32, kernel_size=4, stride=2, padding=1)
+        # H = get_conv_output_size(H, 4, stride=2, padding=1)
+        # W = get_conv_output_size(W, 4, stride=2, padding=1)
+        self.num_conv2_features = C * H * W
 
-        self.fc1 = nn.Linear(32 * H * W, 512)
+        # self.fc1 = nn.Linear(32 * H * W, 512)
+        self.fc1 = nn.Linear(C * H * W, 48)
 
         # Value layers
-        self.vl1 = nn.Linear(512, 128)
-        self.vl2 = nn.Linear(128, 1)
+        self.vl1 = nn.Linear(48, 12)
+        self.vl2 = nn.Linear(12, 1)
 
         # Action advantage layers
-        self.al1 = nn.Linear(512, 128)
-        self.al2 = nn.Linear(128, num_actions)
+        self.al1 = nn.Linear(48, 12)
+        self.al2 = nn.Linear(12, num_actions)
 
     def forward(self, x):
-        out = F.relu(self.conv1(x))
-        out = F.relu(self.conv2(out))
-        out = out.view(-1, self.num_conv2_features)
+        # out = F.relu(self.conv1(x))
+        # out = F.relu(self.conv2(out))
+        out = x.view(-1, self.num_conv2_features)
         out = F.relu(self.fc1(out))
 
         value = F.relu(self.vl1(out))
--- src/trainer/single.py
+++ src/trainer/single.py
@@ -5,6 +5,7 @@ import gc
 import os
 import psutil
 import time
+import gym
 
 import torch
 import torch.optim as optim
@@ -23,12 +24,14 @@ EPS_END = 0.05
 EPS_DECAY_STEPS = 5_000_000
 LEARNING_RATE = 0.00025
 FRAME_STACKING = 4
-NUM_ACTIONS = 4
+# NUM_ACTIONS = 4
+NUM_ACTIONS = 2
 MEMORY_CAPACITY = 500_000
 MIN_MEMORY_SIZE = 10_000
 TARGET_NET_UPDATE_FREQ = 10_000
 NUM_EPISODES = 15_000
-C, H, W = 1, 90, 120
+# C, H, W = 1, 90, 120
+C, H, W = 1, 1, 4
 MULTI_STEP = 3
 
 
@@ -64,8 +67,9 @@ def main():
         else:
             return torch.randint(0, NUM_ACTIONS, (1, 1), device=device, dtype=torch.long)
 
-    env = Environment()
+    env = gym.make('CartPole-v0')
     step = 0
+    done = False
 
     for i_episode in range(NUM_EPISODES):
         gpu_mem = torch.cuda.memory_allocated(device) / 1e6
@@ -78,9 +82,11 @@ def main():
         raw_obs = env.reset()
         stacking.reset()
 
-        raw_obs = toTensor(raw_obs).to(device)
-        raw_obs = raw_obs.unsqueeze(0)
-        transformed_obs = transform(raw_obs)
+        # raw_obs = toTensor(raw_obs).to(device)
+        # raw_obs = raw_obs.unsqueeze(0)
+        # transformed_obs = transform(raw_obs)
+        transformed_obs = torch.tensor(
+            raw_obs, dtype=torch.float).view(1, 1, 1, 4)
         stacked_obs = stacking(transformed_obs)
 
         for step in count(step):
@@ -90,16 +96,18 @@ def main():
             for step in range(step, step + action_repeat):
 
                 # Play an action in the game and get the next observation
-                next_raw_obs, reward, done = env.step(action.item())
+                next_raw_obs, reward, done, _ = env.step(action.item())
                 reward = torch.tensor(
                     [[reward]], dtype=torch.float, device=device)
 
                 if done:
                     next_stacked_obs = None
                 else:
-                    next_raw_obs = toTensor(next_raw_obs).to(device)
-                    next_raw_obs = next_raw_obs.unsqueeze(0)
-                    next_transformed_obs = transform(next_raw_obs)
+                    # next_raw_obs = toTensor(next_raw_obs).to(device)
+                    # next_raw_obs = next_raw_obs.unsqueeze(0)
+                    # next_transformed_obs = transform(next_raw_obs)
+                    next_transformed_obs = torch.tensor(
+                        next_raw_obs, dtype=torch.float).view(1, 1, 1, 4)
                     next_stacked_obs = stacking(next_transformed_obs)
 
                 transition = Transition(
@@ -112,6 +120,8 @@ def main():
                 stacked_obs = next_stacked_obs
 
                 if len(memory) < MIN_MEMORY_SIZE:
+                    if done:
+                        break
                     continue
 
                 transitions, sample_ids, is_weights = stacking.from_memory(
@@ -120,7 +130,7 @@ def main():
 
                 pred, target = get_prediction_and_target(
                     batch, online_dqn, target_dqn,
-                    batch_size=BATCH_SIZE, gamma=GAMMA, 
+                    batch_size=BATCH_SIZE, gamma=GAMMA,
                     multi_step=MULTI_STEP, device=device)
 
                 errors = torch.abs(pred - target)
@@ -145,28 +155,34 @@ def main():
                         raw_obs = env.reset()
                         stacking.reset()
 
-                        raw_obs = toTensor(raw_obs).to(device)
-                        transformed_obs = transform(raw_obs).unsqueeze(0)
+                        # raw_obs = toTensor(raw_obs).to(device)
+                        # raw_obs = raw_obs.unsqueeze(0)
+                        # transformed_obs = transform(raw_obs)
+                        transformed_obs = torch.tensor(
+                            raw_obs, dtype=torch.float).view(1, 1, 1, 4)
                         stacked_obs = stacking(transformed_obs)
 
                         mean_max_q_value = 0
+                        rewards = 0
                         for t in count():
                             q_values = online_dqn(stacked_obs)
                             action = q_values.argmax().view(1, 1)
                             mean_max_q_value += q_values.max().item()
 
-                            next_raw_obs, reward, done = env.step(
+                            next_raw_obs, reward, done, _ = env.step(
                                 action.item())
+                            rewards += reward
                             reward = torch.tensor(
                                 [[reward]], dtype=torch.float, device=device)
 
                             if done:
                                 next_stacked_obs = None
                             else:
-                                next_raw_obs = toTensor(
-                                    next_raw_obs).to(device)
-                                next_transformed_obs = transform(
-                                    next_raw_obs).unsqueeze(0)
+                                # next_raw_obs = toTensor(next_raw_obs).to(device)
+                                # next_raw_obs = next_raw_obs.unsqueeze(0)
+                                # next_transformed_obs = transform(next_raw_obs)
+                                next_transformed_obs = torch.tensor(
+                                    next_raw_obs, dtype=torch.float).view(1, 1, 1, 4)
                                 next_stacked_obs = stacking(
                                     next_transformed_obs)
 
@@ -183,7 +199,7 @@ def main():
                                 break
 
                         mean_q_value = mean_max_q_value / (t + 1)
-                        print("episode score:", str(env.game.score),
+                        print("episode score:", str(rewards),
                               f"\tmean q-value: {mean_q_value:.4g}", "\tat episode", i_episode)
 
                     if i_episode % 20 == 19:
@@ -195,7 +211,7 @@ def main():
 
                     # IMPORTANT: Stop the episode when done
                     break
-            
+
             if done:
                 break
 
